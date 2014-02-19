b9d4e1fe3826b2c9d7d3b82f763ba4edf5d81a7f
David <david.aghassi@gmail.com>
2014-02-19 09:24:34 -0500

    Added a health bar
    I don’t know if it functions yet, but it shows up.

---
+++ Assets/Character/Health/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	
+	public float progress = 0.75f;
+	public Vector2 pos = new Vector2(0.67f, 0.02f);
+	public Vector2 size = new Vector2(256,256);
+	public Texture2D emptyTex;
+	public Texture2D fullTex;
+	
+	void OnGUI() {
+		
+		float posX = Screen.width * pos.x;
+		float posY = Screen.height * pos.y;
+		
+		//draw the background:
+		GUI.BeginGroup(new Rect(posX, posY, size.x, size.y));
+		GUI.DrawTexture(new Rect(0,0, size.x, size.y), emptyTex);
+		
+		//draw the filled-in part:
+		//GUI.BeginGroup(new Rect(0, 0, size.x * progress, size.y));
+		//GUI.DrawTexture(new Rect(0,0, size.x, size.y), fullTex);
+		//draw the filled-in part FLIPPED:
+		int xProg = (int)(size.x * progress);
+		GUI.BeginGroup(new Rect(size.x - xProg, 0, xProg, size.y));
+		GUI.DrawTexture(new Rect(-size.x + xProg,0, size.x, size.y), fullTex);
+		
+		GUI.EndGroup();
+		GUI.EndGroup();
+	}
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	}
+}
