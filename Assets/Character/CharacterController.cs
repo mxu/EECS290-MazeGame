c27da93263567d76cdb24cb94ad4221aef6af7c5
David <david.aghassi@gmail.com>
2014-02-19 20:23:42 -0500

    Added a health bar and a character controller
    The character controller deals with if the player is hit and can deduct
    health. Need monsters to test with.

---
+++ Assets/Character/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.transform.tag == "Monster"){
+			dealDamage(.5f);
+			Debug.Log ("The player has been hit!");
+		}
+	}
+
+	//Deals damage to the player
+	static void dealDamage(float damage){
+		HealthBar.progress -= damage;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
