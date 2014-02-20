906ca793f8884e2ff302be4a55b5e923115ac713
David <david.aghassi@gmail.com>
2014-02-19 20:24:00 -0500

    Revert "Added a health bar and a character controller"
    This reverts commit c27da93263567d76cdb24cb94ad4221aef6af7c5.

--- Assets/Character/Health/HealthBar.cs
+++ Assets/Character/Health/HealthBar.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour {
 	
-	public static float progress = 100.0f;
-	public Vector2 pos = new Vector2(0f, 0.02f);
+	public float progress = 0.75f;
+	public Vector2 pos = new Vector2(0.67f, 0.02f);
 	public Vector2 size = new Vector2(256,256);
 	public Texture2D emptyTex;
 	public Texture2D fullTex;
 	
 	void OnGUI() {
+		
 		float posX = Screen.width * pos.x;
 		float posY = Screen.height * pos.y;
 		
@@ -32,8 +33,7 @@ public class HealthBar : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	}
-
-
+	
 	// Update is called once per frame
 	void Update() {
 	}
