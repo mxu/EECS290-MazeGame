bb0869e41072e187f197036220b1ab5c88b586b9
David <david.aghassi@gmail.com>
2014-02-19 23:01:25 -0500

    Double checking I actually recovered everything.

--- Assets/Character/Health/HealthBar.cs
+++ Assets/Character/Health/HealthBar.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour {
 	
-	public float progress = 0.75f;
-	public Vector2 pos = new Vector2(0.67f, 0.02f);
+	public static float progress = 100.0f;
+	public Vector2 pos = new Vector2(0f, 0.02f);
 	public Vector2 size = new Vector2(256,256);
 	public Texture2D emptyTex;
 	public Texture2D fullTex;
 	
 	void OnGUI() {
-		
 		float posX = Screen.width * pos.x;
 		float posY = Screen.height * pos.y;
 		
@@ -33,7 +32,8 @@ public class HealthBar : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	}
-	
+
+
 	// Update is called once per frame
 	void Update() {
 	}
